082b77776cb09d80d9d494a04165b84a3734a980
Jonathan Frazier <jjfraz11@gmail.com>
2014-07-30 23:26:46 -0400

    Use Dexie for indexeddb_service.

--- extension/app/component/utilities/indexeddb_service.js
+++ extension/app/component/utilities/indexeddb_service.js
@@ -2,115 +2,41 @@
 
 (function(){
   angular.module('LS.utilities').
-    factory('DB', [ '$q', IndexedDB ]);
+    factory('DB', [ IndexedDB ]);
 
-  // Database Services
-  function IndexedDB($q) {
-    var onStoreReady = function() {
-      console.log('Store ready: ' + this.storeName);
-    }
+  function IndexedDB() {
+    var db = new Dexie('linkstreme');
+    db.version(1).stores({
+      links: '++id,&streme_uri_key,streme_id,uri_id',
+      stremes: '++id,&name',
+      uris: '++id,&url'
+    });
 
-    var stores = {}
+    var Uri = db.uris.defineClass({
+      url: String
+    });
 
-    var getStore = function(storeName) {
-      if ( storeName == 'links' ) {
-        return stores['links']
-      } else if ( storeName == 'stremes' ) {
-        return stores['stremes'];
-      } else if ( storeName == 'uris' ) {
-        return stores['uris'];
-      } else { alert( 'Unknown store: ' + storeName); }
-    }
+    var Link = db.links.defineClass({
+      streme_uri_key: String,
+      streme_id: Number,
+      streme_name: String,
+      uri_id: Number,
+      uri_url: String
+    });
 
-    function Store(storeName) {
-      this.store = getStore(storeName);
+    var Streme = db.stremes.defineClass({
+      name: String,
+      links: [ Link ]
+    });
 
-      this.objectName = function() {
-        alert('This method should be overridden by subclasses.');
-      };
+    db.open();
 
-      this.setupNew = function() {
-        alert('This method should be overridden by subclasses.');
-      };
-    }
-
-    Store.prototype.put = function(objectData) {
-      var deferred = $q.defer();
-      var object = this.setupNew(objectData);
-      var self = this;
-
-      this.store.put(object, function(keyPath) {
-        var objectName = self.objectName(object);
-        console.log('Added ' + objectName + ' to ' +
-                    self.store.storeName + ' : ' + keyPath);
-        deferred.resolve(keyPath);
-      }, function() {
-        var objectInfo = JSON.stringify(object);
-        deferred.reject('Could not add ' + objectInfo +
-                        ' to ' + self.store.storeName);
-      });
-
-      return deferred.promise;
-    };
-
-    Store.prototype.get = function(keyPath) {
-      var deferred = $q.defer();
-      var self = this;
-
-      if(!keyPath) { alert('Missing keypath: ' + JSON.stringify(keyPath)); }
-
-      this.store.get(keyPath, function(object) {
-        deferred.resolve(object);
-      }, function() {
-        deferred.reject('No object found in ' + self.store.storeName +
-                        'with keyPath: ' + keyPath);
-      });
-
-      return deferred.promise;
-    };
-
-    Store.prototype.getAll = function() {
-      var deferred = $q.defer();
-      var self = this;
-
-      this.store.getAll(function(objects) {
-        deferred.resolve(objects);
-      }, function() {
-        deferred.reject('Could not retrive objects from ' + self.store.storeName);
-      });
-
-      return deferred.promise;
-    };
-
-    Store.prototype.query = function(queryOptions) {
-      var deferred = $q.defer();
-
-      if(queryOptions.keyRange) {
-        var keyRangeOptions = queryOptions.keyRange;
-        queryOptions.keyRange = this.store.makeKeyRange(keyRangeOptions);
-
-        this.store.query( function(objects) { deferred.resolve(objects); },
-                          queryOptions );
-      } else {
-        deferred.reject('Must set a keyRange in queryOptions.');
-      }
-
-      return deferred.promise;
-    };
-
-    Store.prototype.deleteDatabase = function() {
-      this.store.deleteDatabase();
-      console.log('Delete database for ' + this.store.storeName);
-    }
-    
     return {
-      Store: Store,
+      Link: Link,
+      Streme: Streme,
+      Uri: Uri,
 
-      buildStore: function(storeName, storeConfig) {
-        storeConfig.storeName = storeName;
-        storeConfig.onStoreReady = onStoreReady;
-        stores[storeName] = new IDBStore(storeConfig);
-      }
+      db: db
     };
   }
 })();
