5131424ab62ee50c88fefd56d01b155ec0693a43
Jonathan Frazier <jjfraz11@gmail.com>
2014-08-05 08:48:11 -0400

    Move state object to seperate factory in shared_service.

--- extension/app/component/linkstreme/shared_service.js
+++ extension/app/component/linkstreme/shared_service.js
@@ -2,108 +2,108 @@
 
 (function(){
   angular.module('LS.services').
-    factory('Shared', [ '$rootScope', 'Data', 'Storage', Shared ]);
+    factory('Shared.State', [ '$q', '$rootScope', 'Storage', SharedState ]).
+    factory('Shared', [ '$rootScope', 'Data', 'Shared.State', 'Storage', Shared ]);
 
-  function Shared($rootScope, Data, Storage) {
+  function SharedState($q, $rootScope, Storage) {
+    var stateHash = {
+      currentStreme: { id: null, name: 'Select Streme...', links: [] },
+      stremeLinks: [],
+      linkTags: { }
+    };
 
-    var state = (function() {
-      var stateHash = {
-        currentStreme: { id: null, name: 'Select Streme...', links: [] },
-        stremeLinks: [],
-        linkTags: { }
-      };
+    var validKey = function(key) {
+      if (angular.toType(key) === 'array') {
+        return true;
+      } else if (angular.toType(key) === 'string') {
+        return true;
+      } else {
+        return false;
+      }
+    };
 
-      var validKey = function(key) {
-        if (angular.toType(key) === 'array') {
-          return true;
-        } else if (angular.toType(key) === 'string') {
-          return true;
-        } else {
-          return false;
-        }
-      };
+    var getKeyName = function(key) {
+      var keyName;
+      if (angular.toType(key) === 'array' ) {
+        keyName = key.join('.');
+      } else if (angular.toType(key) === 'string' ) {
+        keyName = key;
+      }
 
-      var getKeyName = function(key) {
-        var keyName;
-        if (angular.toType(key) === 'array' ) {
-          keyName = key.join('.');
-        } else if (angular.toType(key) === 'string' ) {
-          keyName = key;
-        }
+      return keyName;
+    };
 
-        return keyName;
-      };
+    var getNestedValue = function(keyArray) {
+      return keyArray.reduce(function(result, innerKey) {
+        return result[innerKey];
+      }, stateHash);
+    };
 
-      var getNestedValue = function(keyArray) {
-        return keyArray.reduce(function(result, innerKey) {
-          return result[innerKey];
-        }, stateHash);
-      };
+    var setNestedValue = function(keyArray, value){
+      var keyArray = keyArray.slice(0);
+      var lastKey = keyArray.pop();
+      keyArray.reduce(function(result,innerKey) {
+        result[innerKey] = result[innerKey] || {};
+        return result[innerKey];
+      }, stateHash)[lastKey] = value;
+    };
 
-      var setNestedValue = function(keyArray, value){
-        var keyArray = keyArray.slice(0);
-        var lastKey = keyArray.pop();
-        keyArray.reduce(function(result,innerKey) {
-          result[innerKey] = result[innerKey] || {};
-          return result[innerKey];
-        }, stateHash)[lastKey] = value;
-      };
+    var get = function(key, callback) {
+      if(!validKey(key)) {
+        alert('Invalid key for SharedState.get: ' + JSON.stringify(key));
+        return false;
+      }
 
-      var get = function(key, callback) {
-        if(!validKey(key)) {
-          alert('Invalid key for state.get: ' + JSON.stringify(key));
-          return false;
-        }
+      var data;
+      if (angular.toType(key) === 'array') {
+        data = getNestedValue(key);
+      } else {
+        data = stateHash[key];
+      }
 
-        var data;
-        if (angular.toType(key) === 'array') {
-          data = getNestedValue(key);
-        } else {
-          data = stateHash[key];
-        }
+      if (callback) { callback(data); }
+      return data;
+    };
 
-        if (callback) { callback(data); }
-        return data;
-      };
+    var set = function(key, data) {
+      if(!validKey(key)) {
+        alert('Invalid key for SharedState.set: ' + JSON.stringify(key));
+        return false;
+      }
 
-      var set = function(key, data) {
-        if(!validKey(key)) {
-          alert('Invalid key for state.set: ' + JSON.stringify(key));
-          return false;
-        }
+      if (angular.toType(key) === 'array') {
+        setNestedValue(key, data);
+      } else {
+        stateHash[key] = data;
+      }
 
-        if (angular.toType(key) === 'array') {
-          setNestedValue(key, data);
-        } else {
-          stateHash[key] = data;
-        }
+      var eventName = getKeyName(key) + '.update';
+      alert(eventName);
+      $rootScope.$broadcast(eventName, data);
+    };
 
-        var eventName = getKeyName(key) + '.update';
-        alert(eventName);
-        $rootScope.$broadcast(eventName, data);
-      };
+    return {
+      get: get,
+      set: set,
 
-      return {
-        get: get,
-        set: set,
+      save: function(key) {
+        var backupData = get(key);
+        var keyName = getKeyName(key);
+        return Storage.set(keyName, backupData);
+      },
 
-        save: function(key) {
-          var backupData = get(key);
-          var keyName = getKeyName(key);
-          return Storage.set(keyName, backupData);
-        },
+      loadSaved: function(key) {
+        var keyName = getKeyName(key);
 
-        loadSaved: function(key) {
-          var keyName = getKeyName(key);
+        return Storage.get(keyName).
+          then(function(data) {
+            set(key, data);
+          }, function(message) { alert(message); });
+      }
+    };
+  }
 
-          return Storage.get(keyName).
-            then(function(data) {
-              set(key, data);
-            }, function(message) { alert(message); });
-        }
-      };
-    })();
-
+  function Shared($rootScope, Data, SharedState, Storage) {
     var register = function(scope, eventName, callback) {
       return scope.$on(eventName, function(event, data) {
         callback(event, data);
@@ -117,8 +117,8 @@
       return Data.findLinksByStremeId(streme_id).
         then(function(foundLinks) {
           if(foundLinks) {
-            state.set(['currentStreme','links'], foundLinks);
-            return state.save('currentStreme');
+            SharedState.set(['currentStreme','links'], foundLinks);
+            return SharedState.save('currentStreme');
           }
         }, function(message) { alert(message); });
     };
@@ -129,9 +129,9 @@
         then(function(foundTags) {
           alert('Tags for ' + link_id + ' : ' + JSON.stringify(foundTags));
           if(foundTags.length > 0) {
-            angular.forEach(state.get(['currentStreme','links']), function(link, index) {
+            angular.forEach(SharedState.get(['currentStreme','links']), function(link, index) {
               if(link.id === link_id) {
-                state.set(['linkTags', link_id], foundTags);
+                SharedState.set(['linkTags', link_id], foundTags);
               }
             });
           }
@@ -144,23 +144,23 @@
     });
 
     register($rootScope, 'currentStreme.links.update', function(event, links) {
-      angular.forEach(state.get(['currentStreme', 'links']), function(link) {
+      angular.forEach(SharedState.get(['currentStreme', 'links']), function(link) {
         if(link.id) { updateLinkTags(link.id); }
       });
     });
 
-    state.loadSaved('currentStreme');
+    SharedState.loadSaved('currentStreme');
 
     return {
-      currentStreme: state.currentStreme,
+      currentStreme: SharedState.currentStreme,
 
-      get: state.get,
-      set: state.set,
+      get: SharedState.get,
+      set: SharedState.set,
 
       register: register,
 
       updateStremeLinks: function() {
-        var streme_id = state.get('currentStreme').id;
+        var streme_id = SharedState.get('currentStreme').id;
         return updateStremeLinks(streme_id)
       },
 
