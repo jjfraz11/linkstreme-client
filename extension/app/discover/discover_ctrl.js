d6e62a45412360689c4d18b1718dae6b60cc5884
Jonathan Frazier <jjfraz11@gmail.com>
2014-12-01 12:39:05 -0500

    Open new tab from link url.

--- extension/app/discover/discover_ctrl.js
+++ extension/app/discover/discover_ctrl.js
@@ -1,29 +1,39 @@
 'use strict';
 
 (function(){
-  angular.module('LS.controllers').
-    controller('DiscoverCtrl', [ '$scope', 'Data', 'Shared', DiscoverCtrl ]);
+    angular.module('LS.controllers').
+	controller('DiscoverCtrl', [ '$scope', 'Data', 'Shared', 'Tabs', DiscoverCtrl ]);
 
-  function DiscoverCtrl($scope, Data, Shared){
-    var loadCurrentStreme = function() {
-      Shared.get('currentStreme', function(currentStreme) {
-        $scope.currentStreme = currentStreme;
-      });
-    };
+    function DiscoverCtrl($scope, Data, Shared, Tabs){
+	var loadCurrentStreme = function() {
+	    Shared.get('currentStreme', function(currentStreme) {
+		$scope.currentStreme = currentStreme;
+	    });
+	};
 
-    $scope.name = 'DiscoverCtrl'
+	$scope.name = 'DiscoverCtrl';
 
-    $scope.removeLink = function(link_id) {
-      Data.removeLink(link_id).
-        then(function() {
-          Shared.updateStremeLinks();
-        }, function(message) { alert(message); });
-    };
+	$scope.openTab = function(linkUrl) {
+	    var tabOptions = {
+		active: false,
+		index:  0,
+		url:    linkUrl
+	    };
 
-    Shared.register($scope, 'currentStreme.links.update', function(event, links) {
-      loadCurrentStreme();
-    });
+	    Tabs.create(tabOptions);
+	};
 
-    loadCurrentStreme();
-  }
+	$scope.removeLink = function(link_id) {
+	    Data.removeLink(link_id).
+		then(function() {
+		    Shared.updateStremeLinks();
+		}, function(message) { alert(message); });
+	};
+
+	Shared.register($scope, 'currentStreme.links.update', function(event, links) {
+	    loadCurrentStreme();
+	});
+
+	loadCurrentStreme();
+    }
 })();
